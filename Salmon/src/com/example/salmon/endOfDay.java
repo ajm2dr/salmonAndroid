d3472b3eadfd7c5a77ee63c783ca9bc90bdc21ce
Joseph Baik <josephsbaik@gmail.com>
2015-02-09 03:13:00 -0500

    All Activities

---
+++ Salmon/src/com/example/salmon/endOfDay.java
@@ -0,0 +1,18 @@
+package com.example.salmon;
+
+import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class endOfDay extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.endofday);
+	}
+}
