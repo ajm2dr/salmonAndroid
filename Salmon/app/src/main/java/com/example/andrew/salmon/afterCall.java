c2bfbefae9a35c952170b6acbbfbfae3257811ec
Andrew Mitchell <ajm2dr@virginia.edu>
2015-02-11 13:21:56 -0500

    Update afterCall.java
    I added Spinners

--- Salmon/app/src/main/java/com/example/andrew/salmon/afterCall.java
+++ Salmon/app/src/main/java/com/example/andrew/salmon/afterCall.java
@@ -26,5 +26,31 @@ public class afterCall extends Activity{
                 afterCall.this.startActivity(i);
             }
         });
+          Spinner spinner = (Spinner) findViewById(R.id.spinner_Q1);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.companion_options, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        Spinner spinner2 = (Spinner) findViewById(R.id.spinner_Q4);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.purpose_list, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner2.setAdapter(adapter2);
+
+        Spinner spinner3 = (Spinner) findViewById(R.id.spinner_Q5);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,
+                R.array.activity_options, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner3.setAdapter(adapter3);
 	}
 }
