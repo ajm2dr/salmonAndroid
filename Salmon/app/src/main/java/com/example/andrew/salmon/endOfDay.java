97e5b73cd24d6f613879d97b8d8d29e8dd57a3c0
Andrew Mitchell <ajm2dr@virginia.edu>
2015-02-12 00:27:33 -0500

    Update endOfDay.java
    added spinners

--- Salmon/app/src/main/java/com/example/andrew/salmon/endOfDay.java
+++ Salmon/app/src/main/java/com/example/andrew/salmon/endOfDay.java
@@ -26,5 +26,22 @@ public class endOfDay extends Activity{
                 endOfDay.this.startActivity(i);
             }
         });
+            Spinner spinner = (Spinner) findViewById(R.id.q9_s1);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.companion_options, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        Spinner spinner2 = (Spinner) findViewById(R.id.q12_s1);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.precentages, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner2.setAdapter(adapter2);
 	}
 }
